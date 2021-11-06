511289aa283139c82c1648593bc044fed52396c9
Glitch (mango-jet) <none>
2021-11-06 22:50:17 +0000

    Update kl_autoeat.lua

--- kl_autoeat.lua
+++ kl_autoeat.lua
@@ -44,7 +44,7 @@ while enabled do
                     end
                end
                local j = player.PlayerGui:WaitForChild("EatFruitBecky").Dialogue.Accept
-               wait(1)
+               wait(1.4)
                eat()
             end
         end
