bc31c43d65bf985902e7fcf28f713fa21f4aa672
Glitch (mango-jet) <none>
2021-11-06 22:42:24 +0000

    auto eat

---
+++ kl_autoeat.lua
@@ -0,0 +1,94 @@
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoid = character:WaitForChild("Humanoid")
+local hrp = character:WaitForChild("HumanoidRootPart")
+
+local vim = game:GetService("VirtualInputManager")
+
+--[[
+
+ALL FRUITS AS OF 04/11/21
+
+AlloFruit
+BarrierFruit
+BombFruit
+BrachioFruit
+BuddhaFruit
+DarkFruit
+DoughFruit
+DragonFruit
+FlameFruit
+FoxFruit
+GasFruit
+GiraffeFruit
+GravityFruit
+GumFruit
+HumanFruit
+IceFruit
+LeopardFruit
+LightFruit
+LoveFruit
+MagmaFruit
+OpFruit
+PawFruit
+PhoenixFruit
+QuakeFruit
+RumbleFruit
+SandFruit
+ShadowFruit
+SnowFruit
+SpinFruit
+SpinoFruit
+SpiritFruit
+SpikeFruit
+StringFruit
+VenomFruit
+WolfFruit
+--]]
+
+
+local function sendMesssage(message, duration)
+    spawn(function ()
+      local CoreGui = game:GetService("StarterGui")
+      CoreGui:SetCore("SendNotification", {
+      Title = "Notification";
+      Text = message;
+      Duration = duration; 
+      Button1 = "ok";
+      Button2 = "stfu";
+      })
+    end)
+  end
+
+local function eat()
+    --1920x1080 no idea if it works for others
+    vim:SendMouseButtonEvent(753,700,0,true,game,0)
+    wait()
+    vim:SendMouseButtonEvent(753,700,0,false,game,0)
+    enabled = false
+    sendMesssage("eaten fruit", 10)
+end
+
+while enabled do
+   wait(0.1)
+   for i,v in pairs(player.Backpack:GetDescendants()) do
+       if v:IsA("Tool") then
+           print("found tool")
+           if v.Name == fruitName then
+               print("found fruit")
+               humanoid:EquipTool(v)
+               for x,g in pairs(character:GetChildren()) do
+                   if g:IsA("Tool") then
+                       if g.Name == fruitName then
+                           g:Activate()
+                           break
+                        end
+                    end
+               end
+               local j = player.PlayerGui:WaitForChild("EatFruitBecky").Dialogue.Accept
+               wait(.6)
+               eat()
+            end
+        end
+    end
+end
