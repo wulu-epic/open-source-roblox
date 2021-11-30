c144140c2c48ac3c1ae8663ab8a836b56ff41298
wulu-epic <70172469+wulu-epic@users.noreply.github.com>
2021-11-30 22:35:24 +0000

    Update king_legacy.lua

--- king_legacy.lua
+++ king_legacy.lua
@@ -7,9 +7,79 @@ local ts = game:GetService("TweenService")
 local tweenInfo = TweenInfo.new(.1,Enum.EasingStyle.Linear,Enum.EasingDirection.Out)
 local vim = game:GetService('VirtualInputManager')
 
+local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
+
+local X = Material.Load({
+	Title = "King Legacy, wulu#0827",
+	Style = 6,
+	SizeX = 500,
+	SizeY = 350,
+	Theme = "Dark",
+	ColorOverrides = {
+		MainFrame = Color3.fromRGB(27, 9, 9)
+	}
+})
+
+local fruits = {
+	"AlloFruit",
+	"BarrierFruit",
+	"BombFruit", 
+	"BrachioFruit", 
+	"BuddhaFruit", 
+	"DarkFruit", 
+	"DoughFruit", 
+	"DragonFruit" , 
+	"FlameFruit" , 
+	"FoxFruit", 
+	"GasFruit", 
+	"GiraffeFruit", 
+	"GravityFruit", 
+	"GumFruit", 
+	"HumanFruit", 
+	"IceFruit", 
+	"LeopardFruit", 
+	"LightFruit", 
+	"LoveFruit", 
+	"MagmaFruit", 
+	"OpFruit", 
+	"PawFruit", 
+	"PhoenixFruit", 
+	"QuakeFruit" , 
+	"RumbleFruit", 
+	"SandFruit", 
+	"ShadowFruit", 
+	"SnowFruit", 
+	"SpinFruit", 
+	"SpinoFruit", 
+	"SpiritFruit" , 
+	"SpikeFruit", 
+	"StringFruit", 
+	"VenomFruit", 
+	"WolfFruit"
+}
+
+local Farms = X.New({
+	Title = "Farms"
+})
+
+local DevilFruit = X.New({
+	Title = "Devil Fruits"
+})
+
+local Settings = X.New({
+	Title = "Settings"
+})
+
+getgenv()._weaponName = ""
 getgenv()._fruitName = "";
+getgenv()._pickupFruit  = false;
 
 getgenv()._autoEatFruit = false;
+
+
+getgenv()._autoAttack = false;
+
+local weapons = {}
 
 local function sendMesssage(message, duration)
     spawn(function ()
@@ -46,6 +116,17 @@ local function clickinstance(a)
     game:GetService("VirtualInputManager"):SendMouseButtonEvent(a.AbsolutePosition.X+a.AbsoluteSize.X/2,a.AbsolutePosition.Y+50,0,false,a,1)
 end
 
+local function getTools() 
+  local unpacked = unpack(table)
+  for i,v in pairs(player.Backpack:GetChildren()) do
+      if v:IsA("Tool") then
+          if unpacked ~= v.Name then
+            table.insert(weapons, v.Name)
+          end
+      end
+  end
+end
+
 local function autoEat()
     spawn(function()
         while _autoEatFruit do
@@ -72,3 +153,58 @@ local function autoEat()
          end
     end)    
 end
+
+local function pickupFruits()
+    spawn(function()
+        while _pickupFruit do
+          for i,v in pairs(workspace:GetChildren()) do
+              if v:IsA("Tool") then
+                  local oldpos = hrp.Position                 
+                  character:PivotTo(v.Handle.Position)
+                  wait(.2)
+                  character:PivotTo(oldpos)
+              end
+          end
+          wait()
+        end
+    end)
+  end
+
+
+local fruitSelector = DevilFruit.Dropdown({
+	Text = "Select a fruit",
+	Callback = function(Value)
+		print(Value)
+	end,
+	Options = {
+		
+	},
+	Menu = {
+		Information = function(self)
+			X.Banner({
+				Text = "For autoeat"
+			})
+		end
+	}
+})
+
+fruitSelector:SetOptions(fruits)
+
+local AutoEat = DevilFruit.Toggle({
+	Text = "Auto Eat",
+	Callback = function(Value)
+		getgenv()._autoEatFruit = Value
+        autoEat()
+	end,
+	Enabled = false
+})
+
+local AutoPickUp = DevilFruit.Toggle({
+	Text = "Auto Pickup",
+	Callback = function(Value)
+		getgenv()._pickupFruit = Value
+        pickupFruits()
+	end,
+	Enabled = false
+})
+
