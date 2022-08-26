6019cd0754b19d802052db84602b19115e7b7b81
coolest person in the world <70172469+wulu-epic@users.noreply.github.com>
2022-08-26 03:12:19 +0100

    Update utils.lua

--- Functional_Scripts/utils.lua
+++ Functional_Scripts/utils.lua
@@ -65,11 +65,11 @@ function utils:consoleUI()
 	frame.Visible = true
 
 	local exit = Instance.new("TextButton", frame)
-	exit.Position = UDim2.new(0,0,1,0)
+	exit.Position = UDim2.new(1,0,1,0)
 	exit.BackgroundColor3 = Color3.fromRGB(255,0,0)
 	exit.TextColor3 = Color3.new(1, 1, 1)
 	exit.Text = "X"
-	exit.Size = UDim2.new(0.05,0,0.1,0)
+	exit.Size = UDim2.new(0.05,0,0.07,0)
 
 	exit.MouseButton1Click:Connect(function()
 		screengui:Destroy()
